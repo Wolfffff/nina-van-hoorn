df0a690b60d82499afd0e127c6b4298f5d2cce89
Wolfffff <22243650+Wolfffff@users.noreply.github.com>
2026-02-09 20:49:36 -0500

    Set base path for GitHub Pages subpath

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/nina-van-hoorn/',
   plugins: [react()],
   build: {
     target: 'esnext',
